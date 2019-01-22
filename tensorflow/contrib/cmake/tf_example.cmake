5fe31bc802c61f2949f8371547b7dd78c2d67efe
hougl <hgl868@126.com>
2019-01-22 12:00:27 +0800

    修改代码

--- tensorflow/contrib/cmake/tf_example.cmake
+++ tensorflow/contrib/cmake/tf_example.cmake
@@ -18,21 +18,10 @@ set(tf_example_example_srcs
 
 add_executable(tf_example_example
     ${tf_example_example_srcs}
-    $<TARGET_OBJECTS:tf_core_lib>
-    $<TARGET_OBJECTS:tf_core_cpu>
-    $<TARGET_OBJECTS:tf_core_framework>
-    $<TARGET_OBJECTS:tf_core_kernels>
-    $<TARGET_OBJECTS:tf_cc_framework>
-    $<TARGET_OBJECTS:tf_cc_ops>
-    $<TARGET_OBJECTS:tf_core_ops>
-    $<TARGET_OBJECTS:tf_core_direct_session>
-    $<$<BOOL:${tensorflow_ENABLE_GPU}>:$<TARGET_OBJECTS:tf_stream_executor>>
 )
 
 target_link_libraries(tf_example_example PUBLIC
-    tf_protos_cc
-    ${tf_core_gpu_kernels_lib}
-    ${tensorflow_EXTERNAL_LIBRARIES}
+    tensorflow
 )
 
 ###########
